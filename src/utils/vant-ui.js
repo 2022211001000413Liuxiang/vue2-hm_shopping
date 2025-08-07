6084b11a8758535b7c226574408862e4c3964ab7
2022211001000413Liuxiang <3259036747@qq.com>
2025-08-07 21:21:29 +0800

    完成首页页面动态渲染

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
-import { Tabbar, TabbarItem, NavBar, Toast } from 'vant'
+import { Search, Swipe, SwipeItem, Grid, GridItem, Tabbar, TabbarItem, NavBar, Toast } from 'vant'
 
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
 Vue.use(NavBar)
 Vue.use(Toast)
+Vue.use(Search)
+Vue.use(Swipe)
+Vue.use(SwipeItem)
+Vue.use(Grid)
+Vue.use(GridItem)
