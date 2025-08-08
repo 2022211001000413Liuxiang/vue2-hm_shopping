1809ede1a5b4a184bb7ae3962222969bfb32b17a
2022211001000413Liuxiang <3259036747@qq.com>
2025-08-08 22:15:01 +0800

    新增搜索历史功能

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,4 +1,5 @@
 const INFO_KEY = 'userInfo'
+const HISTORY_KEY = 'searchHistory'
 export const getInfo = () => {
   const defaultObj = { token: '', userId: '' }
   const result = localStorage.getItem(INFO_KEY)
@@ -11,4 +12,13 @@ export const setInfo = (obj) => {
 
 export const removeInfo = () => {
   localStorage.removeItem(INFO_KEY)
+}
+
+export const getHistory = () => {
+  const result = localStorage.getItem(HISTORY_KEY)
+  return result ? JSON.parse(result) : []
+}
+
+export const setHistory = (arr) => {
+  localStorage.setItem(HISTORY_KEY, JSON.stringify(arr))
 }
--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Search, Swipe, SwipeItem, Grid, GridItem, Tabbar, TabbarItem, NavBar, Toast } from 'vant'
+import { Search, Swipe, SwipeItem, Grid, GridItem, Tabbar, TabbarItem, NavBar, Toast, Icon } from 'vant'
 
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
@@ -10,3 +10,4 @@ Vue.use(Swipe)
 Vue.use(SwipeItem)
 Vue.use(Grid)
 Vue.use(GridItem)
+Vue.use(Icon)
